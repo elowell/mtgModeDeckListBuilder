68695020d1356887746558f1d89b678a305c31f2
Evan Lowell <elowell7@gmail.com>
2020-09-11 21:55:42 -0700

    el: recompiled exe file with updates

--- modeDeckListBuilder.py
+++ modeDeckListBuilder.py
@@ -9,6 +9,17 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup as BS
 from selenium import webdriver
 import time
+import os, os.path
+import errno
+
+# Taken from https://stackoverflow.com/a/600612/119527
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
 
 def get_deck_links(site, commander, decks, titles, themes):
     if site.lower() == 'archidekt':
